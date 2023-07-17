ae6ee2c0788c3513301ea41cbcc6d82f1a7e14cd
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2023-07-17 06:32:41 -0700

    replaced version.bat with version.c version.bat turns out to be problematic on the non-US locales platforms because date formatting is different in different locales.

--- scripts/prebuild.bat
+++ scripts/prebuild.bat
@@ -3,6 +3,5 @@ pushd ..
 call scripts\download.bat stb_image.h https://raw.githubusercontent.com/nothings/stb/master/stb_image.h
 call scripts\download.bat crt.h       https://raw.githubusercontent.com/leok7v/quick.h/main/crt.h
 call scripts\download.bat quick.h     https://raw.githubusercontent.com/leok7v/quick.h/main/quick.h
-call scripts\version.bat > version.h 2>nul
 popd
 exit /b 0
