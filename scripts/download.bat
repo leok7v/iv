1c311f0c2a096067e07e1db4d2af076b06d9dc44
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2023-07-17 06:34:31 -0700

    download.bat added cURL flags --ssl-no-revoke is there because github CDN certificate appears to be revoked when wrapped into Kaspersky anti-virus certificate envelope that is revoked at the moment of this commit --location is actually "follow redirect"

--- scripts/download.bat
+++ scripts/download.bat
@@ -1,5 +1,7 @@
 @echo off
 if not exist "%1" (
-  echo downloading "%1" from "%2" 
-  curl.exe --silent "%2" --create-dirs --output "%1" 
+  echo downloading "%1" from "%2"
+  rem cURL --ssl-no-revoke is there because github CDN certificate is revoked
+  rem cURL --location is actually "follow redirect"
+  curl.exe --ssl-no-revoke --silent --location "%2" --create-dirs --output "%1"
 )
